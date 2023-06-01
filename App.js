ca7009b64146e5124c539c29395efcfe43244510
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-06-01 22:14:18 +0100

    hh

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, withRouter, Route, BrowserRouter as Router } from "react-router-dom";
+import WidgetHome from "./widgetHome";
+import CardDetails from "./scene/cardDetails";
+import Otp from "./scene/otp";
+import Success from "./scene/success";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+// const queryClient = new QueryClient()
+
+
+
+
+const App = () => {
+
+ 
+  
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<WidgetHome />}>
+            <Route index element={<CardDetails />} />
+            <Route path="/index/cardDetails" element={<CardDetails />} />
+            <Route path="/index/otp" element={<Otp />} />
+            <Route path="/index/success" element={<Success />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ widgetHome.js
@@ -0,0 +1,80 @@
+import React, {useState} from "react";
+import SideBar from "./component/sideBar";
+import Modal from "./component/Modal";
+import { Navigate, Outlet, redirect } from "react-router-dom";
+
+
+const WidgetHome = () => {
+  useEffect(() => {
+    // Grab the URL parameters
+    const params = new URLSearchParams(window.location.search);
+    const publicKey = params.get("publicKey");
+    const secretKey = params.get("secretKey");
+    const amount = params.get("amount");
+    const currency = params.get("currency");
+
+    // Process the payment using the retrieved details
+   
+    processPayment(publicKey, secretKey, amount, currency);
+  }, []);
+
+function processPayment(publicKey, secretKey, amount, currency) {
+   
+  // You can use payment APIs or any other payment processing methods here
+
+  // Example code to log the payment details
+  console.log("Payment details:");
+  console.log("Public Key:", publicKey);
+  console.log("Secret Key:", secretKey);
+  console.log("Amount:", amount);
+  console.log("Currency:", currency);
+  }
+
+
+  const [isOpen, setIsOpen] = useState(true);
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Modal isOpen={isOpen} onClose={handleCloseModal}>
+        <div className="inline-block relative overflow-hidden text-left align-bottom transition-all transform bg-[white] rounded-2xl shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <svg
+            onClick={handleCloseModal}
+            className="cursor-pointer absolute right-1 top-2 "
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.9497 7.05032L7.05021 16.9498"
+              stroke="#171717"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M7.05029 7.05032L16.9498 16.9498"
+              stroke="#171717"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+
+          <div className=" md:mt-0 md:col-span-2">
+            <div className="flex">
+              <SideBar />
+              <Outlet/>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default WidgetHome;
